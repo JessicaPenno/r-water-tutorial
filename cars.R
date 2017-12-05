863c1802dc5d08031b25551c5035cc496d72d9a0
Penno <JPENNO@BCGOV>
2017-12-05 15:19:37 -0700

    add summary of mtcars

--- cars.R
+++ cars.R
@@ -2,3 +2,11 @@ library(dplyr)
 library(ggplot2)
 data("mtcars")
 glimpse(mtcars)
+
+
+mt_cars_summary <- mtcars %>%
+  group_by(cyl) %>%
+  summarise(mean_disp = mean(disp),
+            mean_hp = mean(hp))
+
+mt_cars_summary
